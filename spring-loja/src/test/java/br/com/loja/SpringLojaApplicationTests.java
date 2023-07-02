a2bc5e97890a39400c150466709aead89bf83990
Edson Nakata <edson.nakata@datagrupo.com.br>
2023-07-01 22:25:21 -0300

    feature: iniciando os primeiros testes de integração

--- spring-loja/src/test/java/br/com/loja/SpringLojaApplicationTests.java
+++ spring-loja/src/test/java/br/com/loja/SpringLojaApplicationTests.java
@@ -1,13 +1,31 @@
 package br.com.loja;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import br.com.loja.entities.Cliente;
+import br.com.loja.services.ClienteService;
+import br.com.loja.utils.DateUtils;
 
 @SpringBootTest
 class SpringLojaApplicationTests {
 
-	@Test
-	void contextLoads() {
+	@Autowired
+	private ClienteService service;
+
+	@Test()
+	void cadastrarClienteComSucesso() {
+		// preparação
+		Cliente cliente = new Cliente();
+		cliente.setCpf("8888888888");
+		cliente.setNome("houahouaho haouhaouhaou");
+		cliente.setDataNascimento(DateUtils.toDate(null, "dd/MM/yyyy"));
+		cliente.setEmail("emailTeste@teste.com");
+
+		// ação
+
+		// validações
 	}
 
 }
