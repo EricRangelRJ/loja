6c7449f5d6268263e56b3797f84680f0704d3be2
Edson Nakata <edson.nakata@datagrupo.com.br>
2023-07-01 22:23:37 -0300

    refactor: parse String date para Date com o formato escolhido pelo usuário

--- spring-loja/src/main/java/br/com/loja/utils/DateUtils.java
+++ spring-loja/src/main/java/br/com/loja/utils/DateUtils.java
@@ -1,5 +1,6 @@
 package br.com.loja.utils;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -7,7 +8,7 @@ import java.util.GregorianCalendar;
 
 public class DateUtils {
 	
-	// converte uma data do tipo String numa data do tipo Date
+		// converte uma data do tipo String numa data do tipo Date
 		public static Date toDate(String date) {
 			
 			// caso seja vazio
@@ -22,6 +23,27 @@ public class DateUtils {
 			//Calendar cal = new GregorianCalendar(ano, mes - 1, dia);
 			Calendar cal = new GregorianCalendar(ano, mes -1,dia);
 			return cal.getTime();
+		}
+
+		/**
+		 * Converte uma String em Date
+		 * @param date Data em String para ser formatada
+		 * @param format Formato da data, por padrão 'yyyy-MM-dd'
+		 * @return um Date com o padrão especificado
+		 */
+		public static Date toDate(String date, String format) {
+			if(format == null) {
+				format = "yyyy-MM-dd";
+			}
+			SimpleDateFormat simpleDateFormat = new SimpleDateFormat(format);
+			Date response;
+			try {
+				response = simpleDateFormat.parse(date);
+				return response;
+			} catch (ParseException e) {
+				e.printStackTrace();
+				throw new RuntimeException("A data não pôde ser convertida.");
+			}
 		}
 
 		// converte uma data do tipo Date numa String
